cdda46e60af93e0ad86c42c0d16fddf68ca8216f
Per Lindgren <per.lindgren@ltu.se>
2020-07-07 21:04:34 +0200

    PRX node added

---
+++ nrf5x-prx/README.md
@@ -0,0 +1,11 @@
+# Example PRX
+
+Simple example of a PRX node.
+
+```
+❯ cargo embed --release  --features 52840
+```
+
+Replace <52840> according to the PRX node architecture.
+
+
--- nrf5x-prx/src/main.rs
+++ nrf5x-prx/src/main.rs
@@ -1,37 +1,6 @@
 #![no_std]
 #![no_main]
 
-/// Configuration macro to be called by the user configuration in `config.rs`.
-///
-/// Expands to yet another `apply_config!` macro that's called from `init` and performs some
-/// hardware initialization based on the config values.
-macro_rules! config {
-    (
-        baudrate = $baudrate:ident;
-        tx_pin = $tx_pin:ident;
-        rx_pin = $rx_pin:ident;
-    ) => {
-        macro_rules! apply_config {
-            ( $p0:ident, $uart:ident ) => {{
-                let rxd = $p0.$rx_pin.into_floating_input().degrade();
-                let txd = $p0.$tx_pin.into_push_pull_output(Level::Low).degrade();
-
-                let pins = uarte::Pins {
-                    rxd,
-                    txd,
-                    cts: None,
-                    rts: None,
-                };
-
-                Uarte::new($uart, pins, Parity::EXCLUDED, Baudrate::$baudrate)
-            }};
-        }
-    };
-}
-
-#[macro_use]
-mod config;
-
 // Import the right HAL/PAC crate, depending on the target chip
 #[cfg(feature = "51")]
 use nrf51_hal as hal;
@@ -57,50 +26,22 @@ use {
     rtt_target::{rprintln, rtt_init_print},
 };
 
-#[cfg(not(feature = "51"))]
-use hal::{
-    pac::UARTE0,
-    uarte::{self, Baudrate, Parity, Uarte},
-};
-
-#[cfg(feature = "51")]
-use hal::{
-    pac::UART0 as UARTE0,
-    uart::{self as uarte, Baudrate, Parity, Uart as Uarte},
-};
-
 const MAX_PAYLOAD_SIZE: u8 = 64;
 const RESP: &'static str = "Hello back";
 
-#[rtfm::app(device = crate::hal::pac, peripherals = true)]
+#[rtic::app(device = crate::hal::pac, peripherals = true)]
 const APP: () = {
     struct Resources {
         esb_app: EsbApp<U1024, U1024>,
         esb_irq: EsbIrq<U1024, U1024, TIMER0, StatePRX>,
         esb_timer: IrqTimer<TIMER0>,
-        serial: Uarte<UARTE0>,
     }
 
     #[init]
     fn init(ctx: init::Context) -> init::LateResources {
         let _clocks = hal::clocks::Clocks::new(ctx.device.CLOCK).enable_ext_hfosc();
         rtt_init_print!();
-        rprintln!("RTX started");
-
-        #[cfg(not(feature = "51"))]
-        let p0 = hal::gpio::p0::Parts::new(ctx.device.P0);
-
-        #[cfg(feature = "51")]
-        let p0 = hal::gpio::p0::Parts::new(ctx.device.GPIO);
-
-        #[cfg(not(feature = "51"))]
-        let uart = ctx.device.UARTE0;
-
-        #[cfg(feature = "51")]
-        let uart = ctx.device.UART0;
-
-        let mut serial = apply_config!(p0, uart);
-        writeln!(serial, "\n--- INIT ---").unwrap();
+        rprintln!("RTX init");
 
         static BUFFER: EsbBuffer<U1024, U1024> = EsbBuffer {
             app_to_radio_buf: BBBuffer(ConstBBBuffer::new()),
@@ -123,12 +64,12 @@ const APP: () = {
             esb_app,
             esb_irq,
             esb_timer,
-            serial,
         }
     }
 
-    #[idle(resources = [serial, esb_app])]
+    #[idle(resources = [esb_app])]
     fn idle(ctx: idle::Context) -> ! {
+        let mut ct_rx: usize = 0;
         let esb_header = EsbHeader::build()
             .max_payload(MAX_PAYLOAD_SIZE)
             .pid(0)
@@ -139,21 +80,18 @@ const APP: () = {
         loop {
             // Did we receive any packet ?
             if let Some(packet) = ctx.resources.esb_app.read_packet() {
-                let payload = core::str::from_utf8(&packet[..]).unwrap();
-                rprintln!("payload {}", payload);
-                //ctx.resources.serial.write_str("Payload: ").unwrap();
-                //let payload = core::str::from_utf8(&packet[..]).unwrap();
-                //ctx.resources.serial.write_str(payload).unwrap();
-                //ctx.resources.serial.write_str(" rssi: ").unwrap();
-                //ctx.resources
-                //    .serial
-                //    .write(packet.get_header().rssi())
-                //    .unwrap();
-                //ctx.resources.serial.write(b'\n').unwrap();
+                ct_rx += 1;
+                let text = core::str::from_utf8(&packet[..]).unwrap();
+                let rssi = packet.get_header().rssi();
+                rprintln!(
+                    "Received PKG #: {} | Payload: {} | rssi: {}",
+                    ct_rx,
+                    text,
+                    rssi
+                );
                 packet.release();
 
                 // Respond in the next transfer
-
                 let mut response = ctx.resources.esb_app.grant_packet(esb_header).unwrap();
                 let length = RESP.as_bytes().len();
                 &response[..length].copy_from_slice(RESP.as_bytes());
@@ -166,12 +104,11 @@ const APP: () = {
     fn radio(ctx: radio::Context) {
         match ctx.resources.esb_irq.radio_interrupt() {
             Err(Error::MaximumAttempts) => {
-                rprintln!("radio MaxAttempts");
+                rprintln!("Radio Interrupt MaximumAttempts");
             }
-            Err(e) => panic!("Found error {:?}", e),
+            Err(e) => panic!("Radio Interrupt Error {:?}", e),
             Ok(_) => {
-                //rprintln!("{:?}", state);
-                rprintln!("radio interrupt ok");
+                rprintln!("Radio Interrupt Ok");
             }
         }
     }
